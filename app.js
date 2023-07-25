ac14e915d936682b6aaf941b88a0e8f493e69c32
User <ebertrb3@gmail.com>
2023-07-25 14:44:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+////ATIVIDADE 1
+//let pessoa = {
+//    nome: "João",
+//    idade: 15,
+//    cidade: "Rio de Janeiro"
+//}
+//
+////ATIVIDADE 2
+//function nomeCidadao(objeto){
+//    return objeto.nome;
+//}
+//console.log(nomeCidadao(pessoa));
+
+
+////ATIVIDADE 3                  INCOMPLETA
+let aluno = {
+   nome: "Pedro",
+   nota1: 10,
+   nota2: 7, 
+   nota3: 8,
+   nota4: 5,
+   nota5: 3.5
+}
+const mediaTotalTurma = (turma) =>{
+
+}
+//ATIVIDADE 4
+//Número é PAR ou ÍMPAR?
+//function main(n){
+//    var result = n % 2;
+//    console.log(result);
+//    if (result == 1){
+//        console.log("O número " + n + " é ÍMPAR.")
+//    }
+//    else{
+//        console.log("O número " + n + " é PAR.")
+//    }
+//
+//}
+//console.log(main(30));
+
+//ATVIDADE 5
+//function far(celsius){
+//    f = (celsius * 9/5) + 32;
+//    return f;
+//}
+//
+//console.log(far(50));
+
+//ATIVIDADE 6
+//Área do retangulo
+//let altura,largura;
+//
+//
+//function area(alt, larg){
+//    a = alt * larg;
+//    console.log("Um retângulo de " + alt + " por " + larg + " tem uma área de " + a);
+//    return a;
+//}
+//area(10, 20);
+//ATIVIDADE 7 // VERIFICAR SE UM NUMERO É PRIMO
+
+
+
+
+//ATIVIDADE 8 // CALCULAR O DESCONTO EM UM PRODUTO
+//FUNÇÃO: RECEBE O PRECO DO PRODUTO E O % DE DESCONTO.    // RETORNA O PREÇO COM DESCONTO
+
+// function desc(preco, desc){
+//     desconto = preco * (desc / 100);
+//     novoPreco = preco - desconto;
+//     console.log("O produto custava R$" + preco + " e com o desconto de " + desc + "%, o valor final é de R$" + novoPreco)
+//     return novoPreco;
+// }
+// desc(100, 10);
